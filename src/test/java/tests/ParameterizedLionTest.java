e16f4cb5a839ab164ead7852bd26249e9aa4cb87
Maksim Radyushin <m.radyushin@yandex.ru>
2022-10-21 14:59:00 +0300

    add ParameterizedLionTest

---
+++ src/test/java/tests/ParameterizedLionTest.java
@@ -0,0 +1,40 @@
+package tests;
+
+import com.example.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class ParameterizedLionTest {
+
+    private final String sex;
+    private final boolean expectedHasMane;
+
+    public ParameterizedLionTest(String sex, boolean expectedHasMane) {
+
+        this.sex = sex;
+        this.expectedHasMane = expectedHasMane;
+
+    }
+
+    @Parameterized.Parameters
+    public static Object[] getData() {
+        return new Object[][] {
+                { "Самец", true},
+                { "Самка", false},
+        };
+    }
+
+    @Test
+    public void doesHaveManeLionValue() throws Exception {
+
+        Lion lion = new Lion(sex);
+
+        boolean actualHasMane = lion.doesHaveMane();
+
+        Assert.assertEquals(expectedHasMane, actualHasMane);
+    }
+}
